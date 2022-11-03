d3e5c6f10c69cfe9fc4b2cc7200d3ba155afdd93
murtrag <lukasz.czernik@gmail.com>
2022-11-03 15:15:14 +0100

    Overall changer

--- backend/main/main/serializers.py
+++ backend/main/main/serializers.py
@@ -5,14 +5,21 @@ from django.contrib.auth.models import User
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
+    username = serializers.ReadOnlyField()
     class Meta:
         model = User
-        fields = ('username', 'email')
+        fields = ('username', 'email', 'first_name', 'last_name', 'password')
 
 class ProfileSerializer(serializers.HyperlinkedModelSerializer):
-    user = UserSerializer(required=True)
+    
+    internal_currency = serializers.ReadOnlyField()
+    user = UserSerializer()
+
     class Meta:
         model = Profile
-        fields = ['phone_number', 'internal_currency', 'user']
+        fields = ['user', 'phone_number', 'internal_currency']
+    # def update(self, *args, **kwargs):
+    #     print(args)
+    #     print(kwargs)
 
 
