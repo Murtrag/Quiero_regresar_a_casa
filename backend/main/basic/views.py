e6ca472f00ff26705c1f81bd471d2297a11b7511
murtrag <lukasz.czernik@gmail.com>
2022-11-08 12:00:19 +0100

    Under construction

--- backend/main/basic/views.py
+++ backend/main/basic/views.py
@@ -3,5 +3,10 @@ from django.http import JsonResponse
 
 def test(request):
     html = {'db': "Hi guys, It works!!! \o/" }
+    return render(
+            request,
+            'errors/under_construction.html',
+            {}
+            )
     return JsonResponse(html)
 
