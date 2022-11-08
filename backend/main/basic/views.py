f1b1afbce6caba75ef1f77d67e3289dcb8852e9b
murtrag <lukasz.czernik@gmail.com>
2022-11-08 10:45:54 +0100

    hello text

--- backend/main/basic/views.py
+++ backend/main/basic/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import JsonResponse
 
 def test(request):
-    html = {'a':1} 
+    html = "Hi guys, It works!!! \o/" 
     return JsonResponse(html)
 
