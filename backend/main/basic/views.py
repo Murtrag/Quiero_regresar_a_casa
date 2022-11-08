6cb8931659ce31bf11fe37207890424177a72dd4
Łukasz Czernik <lukasz.czernik@gmail.com>
2022-11-08 14:52:52 +0000

    Add static articles model and admin

--- backend/main/basic/views.py
+++ backend/main/basic/views.py
@@ -1,12 +1,7 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 
-def test(request):
-    html = {'db': "Hi guys, It works!!! \o/" }
-    return render(
-            request,
-            'errors/under_construction.html',
-            {}
-            )
-    return JsonResponse(html)
 
+def test(request):
+    return render(request, 'errors/under_construction.html', {})
+    return JsonResponse(html)
--- backend/main/main/models/main.py
+++ backend/main/main/models/main.py
@@ -4,10 +4,29 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from phonenumber_field.modelfields import PhoneNumberField
 
+
+class Country(models.Model):
+    country_code = models.CharField(max_length=5)
+    country_name = models.CharField(max_length=80)
+
+    def __str__(self):
+        return self.country_code
+
+
+class Language(models.Model):
+    country_code = models.CharField(max_length=5)
+    country_name = models.CharField(max_length=80)
+
+    def __str__(self):
+        return self.country_code
+
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     phone_number = PhoneNumberField(blank=True)
-    internal_currency = models.DecimalField(default=0, max_digits=5, decimal_places=2)
+    internal_currency = models.DecimalField(default=0,
+                                            max_digits=5,
+                                            decimal_places=2)
 
     # class Meta:
     #     app_label = 'Authentication and Authorization'
@@ -15,10 +34,12 @@ class Profile(models.Model):
     def __str__(self):
         return f'{self.user.pk}: {self.user.username}'
 
+
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
+
 
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
