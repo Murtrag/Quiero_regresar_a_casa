2392862c864fddacafc34900933e080f1d617fff
murtrag <lukasz.czernik@gmail.com>
2022-11-08 10:53:32 +0100

    ff

--- backend/main/basic/views.py
+++ backend/main/basic/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import JsonResponse
 
 def test(request):
-    html = "Hi guys, It works!!! \o/" 
+    html = {'db': "Hi guys, It works!!! \o/" }
     return JsonResponse(html)
 
