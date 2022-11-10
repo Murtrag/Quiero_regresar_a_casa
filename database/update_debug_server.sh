9f43341fdb3d1e3843a670749cd5c9c39f35ba2b
murtrag <lukasz.czernik@gmail.com>
2022-11-10 23:35:43 +0100

    update comment

--- database/update_debug_server.sh
+++ database/update_debug_server.sh
@@ -1,4 +1,4 @@
-# 0 * * * * sh ~/www/Quiero_regresar_a_casa/database/update_debug_server &
+# 0 * * * * sh ~/database/Quiero_regresar_a_casa/database/update_debug_server &
 cd ~/www/Quiero_regresar_a_casa/backend;
 git pull
 docker restart database_db_1 &
