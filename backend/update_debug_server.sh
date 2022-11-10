6f731e51294c33bd5d835d2b5d47b8caafede67e
murtrag <lukasz.czernik@gmail.com>
2022-11-10 19:44:59 +0100

    ff

--- backend/update_debug_server.sh
+++ backend/update_debug_server.sh
@@ -1,6 +1,5 @@
 # 0 * * * * sh ~/www/Quiero_regresar_a_casa/backend/update_debug_server &
 cd ~/www/Quiero_regresar_a_casa/backend;
 git pull
-source ~/www/Quiero_regresar_a_casa/backend/env/bin/activate
-python3 main/manage.py migrate
+docker exec -it backend_app_1 ./main/manage.py migrate
 docker restart backend_app_1 &
