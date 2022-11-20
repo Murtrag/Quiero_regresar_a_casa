3ff3f04e9a53f22e4f927736ff64d765980ea1cd
murtrag <lukasz.czernik@gmail.com>
2022-11-20 11:10:29 +0100

    ff

--- frontend/main/src/routes.js
+++ frontend/main/src/routes.js
@@ -40,7 +40,7 @@ Coded by www.creative-tim.com
 import Icon from "@mui/material/Icon";
 
 // @mui icons
-import GitHubIcon from "@mui/icons-material/GitHub";
+import VolunteerActivismIcon from '@mui/icons-material/VolunteerActivism';
 
 // Pages
 import AboutUs from "layouts/pages/landing-pages/about-us";
@@ -71,7 +71,7 @@ import Typography from "layouts/sections/elements/typography";
 
 const routes = [
   {
-    name: "pages",
+    name: "Panel:",
     icon: <Icon>dashboard</Icon>,
     columns: 1,
     rowsPerColumn: 2,
@@ -240,7 +240,7 @@ const routes = [
     ],
   },
   {
-    name: "docs",
+    name: "organization",
     icon: <Icon>article</Icon>,
     collapse: [
       {
@@ -266,8 +266,34 @@ const routes = [
     ],
   },
   {
-    name: "github",
-    icon: <GitHubIcon />,
+    name: "articles",
+    icon: <Icon>article</Icon>,
+    collapse: [
+      {
+        name: "getting started",
+        description: "All about overview, quick start, license and contents",
+        href: "https://www.creative-tim.com/learning-lab/react/quick-start/material-kit/",
+      },
+      {
+        name: "foundation",
+        description: "See our colors, icons and typography",
+        href: "https://www.creative-tim.com/learning-lab/react/colors/material-kit/",
+      },
+      {
+        name: "components",
+        description: "Explore our collection of fully designed components",
+        href: "https://www.creative-tim.com/learning-lab/react/alerts/material-kit/",
+      },
+      {
+        name: "plugins",
+        description: "Check how you can integrate our plugins",
+        href: "https://www.creative-tim.com/learning-lab/react/datepicker/material-kit/",
+      },
+    ],
+  },
+  {
+    name: "Support us",
+    icon: <VolunteerActivismIcon  />,
     href: "https://www.github.com/creativetimofficial/material-kit-react",
   },
 ];
