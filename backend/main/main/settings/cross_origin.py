671f3b8b36441c891a63b7e868c2785182998ad0
murtrag <lukasz.czernik@gmail.com>
2022-11-24 11:42:12 +0100

    ff

--- backend/main/main/settings/cross_origin.py
+++ backend/main/main/settings/cross_origin.py
@@ -11,4 +11,4 @@ if DEBUG is True:
     ALLOWED_HOSTS.append('*')
     CORS_ORIGIN_ALLOW_ALL = True
 else:
-    CORS_ORIGIN_WHITELIST = ('http://quiero-regresar.mx:81', )
+    CORS_ORIGIN_WHITELIST = ('http://quiero-regresar.mx', )
