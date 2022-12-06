548cc1922455ca5f75bcf2a42518a1f40853f7cf
Lukasz <lukasz.czernik@gmail.com>
2022-12-06 10:34:01 +0000

    At work

--- backend/main/main/tests/fixtures/fixtures.py
+++ backend/main/main/tests/fixtures/fixtures.py
@@ -4,16 +4,25 @@ from pytest_factoryboy import register
 from main.tests.factories import (
     FooterFactory,
     HeaderFactory,
+    ItemFactory
     )
 
 
 register(FooterFactory) # --> footer_factory
-register(HeaderFactory) # --> footer_factory
+register(HeaderFactory) # --> header_factory
+register(ItemFactory) # --> item_factory
 
 @pytest.fixture
-def menu_set(header_factory): # --> generator
+def items_set(item_factory): # --> generator
+    return (
+            item_factory() for _ in range(0, 10)
+            )
+
+@pytest.fixture
+def menu_set(header_factory, items_set): # --> generator
     return(
-        header_factory() for _ in range(0, 10)
+        header_factory.create(items=items_set)
+        for _ in range(0, 10)
     )
 
 @pytest.fixture
--- backend/main/main/tests/units/main_models/test_main_models.py
+++ backend/main/main/tests/units/main_models/test_main_models.py
@@ -1,13 +1,16 @@
 import pytest
-# from main.models import (
-
-# )
+from main.models import (
+        Footer
+        )
 
 @pytest.mark.main
 @pytest.mark.models
 @pytest.mark.xfail
-def test_footer_check_header():
+def test_footer_check_header(db, footer_factory):
     pass
+    for _ in range(0, 10):
+        footer = footer_factory()
+        assert Footer.objects.filter(pk=footer.pk)
 
 
 @pytest.mark.main
@@ -15,6 +18,9 @@ def test_footer_check_header():
 @pytest.mark.xfail
 def test_footer_check_link():
     pass
+    for _ in range(0, 10):
+        footer = footer_factory()
+        assert Footer.objects.filter(pk=footer.pk)
 
 
 @pytest.mark.main
@@ -22,6 +28,9 @@ def test_footer_check_link():
 @pytest.mark.xfail
 def test_footer_check_brand():
     pass
+    for _ in range(0, 10):
+        footer = footer_factory()
+        assert Footer.objects.filter(pk=footer.pk)
 
 
 @pytest.mark.main
@@ -29,3 +38,7 @@ def test_footer_check_brand():
 @pytest.mark.xfail
 def test_footer_check_footer():
     pass
+    for _ in range(0, 10):
+        footer = footer_factory()
+        assert Footer.objects.filter(pk=footer.pk)
+
