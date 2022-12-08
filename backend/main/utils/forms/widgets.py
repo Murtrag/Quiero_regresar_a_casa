e96a7ea39bb6582a25f9abb67465dbd8e13dcdc9
murtrag <lukasz.czernik@gmail.com>
2022-12-08 13:47:44 +0100

    ff

--- backend/main/utils/forms/widgets.py
+++ backend/main/utils/forms/widgets.py
@@ -4,10 +4,18 @@ from django.forms.utils import flatatt
 from django.utils.encoding import force_str
 
 class HrefWidget(Widget):
-    def __init__(self, attrs=None):
-        default_attrs = {'cols': '40', 'rows': '10'}
+    def __init__(self, related_objects=None, attrs=None):
+        default_attrs = {
+                'cols': '40',
+                'rows': '10'
+                }
+
         if attrs:
             default_attrs.update(attrs)
+
+        if related_objects is None:
+            related_objects = list
+
         super().__init__(default_attrs)
 
     def render(self, name, value,renderer, attrs=None):
@@ -17,4 +25,17 @@ class HrefWidget(Widget):
                            flatatt(final_attrs),
                            force_str(value))
 
+    def render_option(self, selected_choices, option_value, option_label):
+        option_value = force_text(option_value)
+        if option_value in selected_choices:
+            selected_html = mark_safe(' selected="selected"')
+            if not self.allow_multiple_selected:
+                # Only allow for a single selection.
+                selected_choices.remove(option_value)
+        else:
+            selected_html = ''
+        return format_html('<option value="{0}"{1}>{2}</option>',
+                           option_value,
+                           selected_html,
+                           force_text(option_label))
 # https://github.com/google/contentbox/blob/master/third_party/django/forms/widgets.py
