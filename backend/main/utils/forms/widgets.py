68961c0cf27a68bdd9b1820269f1e78af31c4fa6
murtrag <lukasz.czernik@gmail.com>
2022-12-13 19:14:15 +0100

    New field works

--- backend/main/utils/forms/widgets.py
+++ backend/main/utils/forms/widgets.py
@@ -6,8 +6,8 @@ from django.utils.encoding import force_str
 class HrefWidget(Widget):
     def __init__(self, related_objects=None, attrs=None):
         default_attrs = {
-                # 'cols': '40',
-                # 'rows': '10'
+                'class': 'vTextField',
+                'autocomplete': 'off'
                 }
 
         if attrs:
@@ -16,26 +16,21 @@ class HrefWidget(Widget):
         if related_objects is None: related_objects = list
         self.options = self.render_option(related_objects)
 
-        super().__init__(default_attrs)
+        super().__init__(attrs=default_attrs)
 
     def render(self, name, value, renderer, attrs=None):
         if value is None: value = ''
-        final_attrs = self.build_attrs(attrs)
+        final_attrs = self.build_attrs(self.attrs, attrs)
         html =  format_html('''
-                <input list="browsers" {} value="{}">
+                <input list="browsers" name="{}" {} value="{}">
                 <datalist id="browsers">
-                ''', final_attrs, force_str(value))
-        html = (
+                ''',name, flatatt(final_attrs), force_str(value))
+        html = [
                 html, 
                 self.options,
                 '</datalist>'
-                )
-        return html.join('\n')
-        
-
-
-
-
+                ]
+        return ''.join(html)
 
     def render_option(self, related_objects):
         option_values =  (list(obj.objects.all()) for obj in related_objects)
@@ -44,9 +39,8 @@ class HrefWidget(Widget):
             option_values.extend(
                     tuple(obj.objects.all())
                     )
-        # print([x.url for x in option_values])
         return "\n".join([
-            format_html('<option value="{}">', option_value.url)
+            format_html('<option value="{}">{}</option>', option_value.url, option_value.title)
             for option_value in option_values
             ])
 
