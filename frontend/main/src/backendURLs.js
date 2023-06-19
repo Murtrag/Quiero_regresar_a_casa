f1ff00200e5b73bcc7c1cf20ff843b27f68b029c
murtrag <lukasz.czernik@gmail.com>
2023-06-19 19:51:14 +0200

    update ip

--- frontend/main/src/backendURLs.js
+++ frontend/main/src/backendURLs.js
@@ -1,7 +1,7 @@
 // const serverURL = "http://quiero-regresar.mx:81/"
 //const serverURL = 'http://127.0.0.1:8000/'
-//const serverURL = "http://136.143.5.43:81/"
-const serverURL = "http://192.168.1.3:8000/"
+const serverURL = "http://136.143.5.51:81/"
+//const serverURL = "http://192.168.1.3:8000/"
 
 // Articles URLs
 const articleDetailURL = ({category, country, language, pk})=>serverURL + `${category}/${country}/${language}/${pk}/`
