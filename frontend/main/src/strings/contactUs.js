059cf0b2740aa4f80ebb05cb40ed2af49c99cbd5
LukaszC <lukasz.czernik@gmail.com>
2023-07-08 23:49:42 +0200

    ff

--- frontend/main/src/strings/contactUs.js
+++ frontend/main/src/strings/contactUs.js
@@ -5,7 +5,7 @@ export default {
 	contactWindow:{
 		title: "Contact Us",
 		text: `For further questions, including partnership opportunities, please email
-                	lukasz.czernik@gmail.com or contact using our contact form.
+			lukasz.czernik@gmail.com or contact using our contact form.
 		`,
 		fields:{
 			name: "Full Name",
@@ -15,16 +15,16 @@ export default {
 		},
 		submit: "SEND MESSAGE"
 	},
-	messageErrors:{
-		serverError:{
-			title: 'Login error',
-			text: data=>`Server message ${JSON.stringify(data)}`,
-		},
-		syntaxError:{
-			title: 'Login error',
-			text: `Syntax error, please contact administrators `,
-		}
-	},
+	// messageErrors:{
+	// 	serverError:{
+	// 		title: 'Login error',
+	// 		text: data=>`Server message ${JSON.stringify(data)}`,
+	// 	},
+	// 	syntaxError:{
+	// 		title: 'Login error',
+	// 		text: `Syntax error, please contact administrators `,
+	// 	}
+	// },
 	fields:{
 		login: "Login",
 		password: "Password",
