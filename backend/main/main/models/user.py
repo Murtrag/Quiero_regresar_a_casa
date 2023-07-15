e20b58a643ca30af5a168fa1a0e03bb472c4af42
murtrag <lukasz.czernik@gmail.com>
2023-07-15 18:15:36 +0200

    ff

--- backend/main/main/models/user.py
+++ backend/main/main/models/user.py
@@ -14,11 +14,11 @@ class Profile(models.Model):
     internal_currency = models.DecimalField(default=0,
                                             max_digits=5,
                                             decimal_places=2)
-    address = models.CharField(max_length=512)
-    city = models.CharField(max_length=255)
-    country = models.CharField(max_length=255)
-    postal_code = models.CharField(max_length=10)
-    profile_description = models.TextField(validators=[MaxLengthValidator(2000)])
+    address = models.CharField(max_length=512, blank=True)
+    city = models.CharField(max_length=255, blank=True)
+    country = models.CharField(max_length=255, blank=True)
+    postal_code = models.CharField(max_length=10, blank=True)
+    profile_description = models.TextField(validators=[MaxLengthValidator(2000)], blank=True)
 
     def __str__(self):
         return f'{self.user.pk}: {self.user.username}'
