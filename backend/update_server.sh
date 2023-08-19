5abccc6e4a4191fefaf371a32095c4dee8b4169b
murtrag <lukasz.czernik@gmail.com>
2023-08-19 02:38:45 +0200

    ff

--- backend/update_server.sh
+++ backend/update_server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# */30 * * * * sh ~/www/Quiero_regresar_a_casa/backend/update_debug_server &
-# @reboot sh ~/www/Quiero_regresar_a_casa/backend/update_debug_server &
+# */30 * * * * sh ~/www/Quiero_regresar_a_casa/backend/update_server &
+# @reboot sh ~/www/Quiero_regresar_a_casa/backend/update_server &
 
 cd ~/www/Quiero_regresar_a_casa/backend;
 ssh-add ~/.ssh/id_rsa;
