14e92043ac21968c87f9a75ee0f49597db54bf7d
murtrag <lukasz.czernik@gmail.com>
2023-08-19 04:11:44 +0200

    update changes

--- frontend/update_server.sh
+++ frontend/update_server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# */30 * * * * sh ~/www/Quiero_regresar_a_casa/frontend/update_debug_server.sh &
-# @reboot sh ~/www/Quiero_regresar_a_casa/frontend/update_debug_server.sh &
+# */30 * * * * sh ~/www/Quiero_regresar_a_casa/frontend/update_server.sh &
+# @reboot sh ~/www/Quiero_regresar_a_casa/frontend/update_server.sh &
 
 cd ~/www/Quiero_regresar_a_casa/frontend;
 ssh-add ~/.ssh/id_rsa;
