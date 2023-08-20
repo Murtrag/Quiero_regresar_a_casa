675e42fb375532ce755622c432cc6c5b7747d0c8
murtrag <lukasz.czernik@gmail.com>
2023-08-20 13:37:01 +0200

    update script

--- scripts/backup_database.sh
+++ scripts/backup_database.sh
@@ -8,13 +8,21 @@ verify_in_crontab "$entry1"
 remote_server="192.168.1.3"
 container_name="backend_app_1"
 backup_file="/tmp/data.json"
+current_datetime=$(date +"%Y-%m-%d_%H-%M-%S")
+host_backup_file="$HOME/db_backups/db_dump-$current_datetime.json"
 
-# Execute the dumpdata command in the container and save it to a file on the remote server
+echo "1. Execute the dumpdata command in the container and save it to a file on the remote server"
 ssh $remote_server "docker exec $container_name python main/manage.py dumpdata > $backup_file"
 
-# Copy the backup file to the local server
-scp "$remote_server:$backup_file" "~/db_backups/data.json"
+# echo "2. Copy the backup file from the container to the host"
+# ssh $remote_server "docker cp $container_name:$backup_file $host_backup_file"
 
-# Optionally, remove the backup file from the remote server
+echo "3. Copy the backup file to the local server"
+rsync -avz murtrag@$remote_server:$backup_file $host_backup_file
+
+
+echo "4. Remove the backup file from the remote server"
 ssh $remote_server "rm -f $backup_file"
+
+
 
