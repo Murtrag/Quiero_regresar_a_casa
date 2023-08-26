83f7f65a2b9b1b2e891323bd1ad03275e64cb254
murtrag <lukasz.czernik@gmail.com>
2023-08-26 20:14:37 +0200

    update backend

--- backend/main/main/settings/route.py
+++ backend/main/main/settings/route.py
@@ -3,6 +3,6 @@ from pathlib import Path
 
 ROOT_URLCONF = 'main.urls'
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = Path.joinpath(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = Path.joinpath(BASE_DIR, 'media/')
