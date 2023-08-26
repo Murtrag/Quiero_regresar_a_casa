05f816a9a6070b67b3d0f585b0199fdab33aa9c4
murtrag <lukasz.czernik@gmail.com>
2023-08-26 16:52:46 +0200

    Update script

--- scripts/setup_env.sh
+++ scripts/setup_env.sh
@@ -13,6 +13,9 @@ function add_alias() {
 # List of aliases to set up
 aliases=(
     "script='cd www/Quiero_regresar_a_casa/scripts/'"
+    "frontend='cd www/Quiero_regresar_a_casa/frontend/'"
+    "backend='cd www/Quiero_regresar_a_casa/backend/'"
+    "db='cd www/Quiero_regresar_a_casa/database/'"
     # Add more aliases here
 )
 
