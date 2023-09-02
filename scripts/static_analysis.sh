7174f9c512610ac51723d07398ed315bbef2cb25
murtrag <lukasz.czernik@gmail.com>
2023-09-02 14:00:25 +0200

    update script

--- scripts/static_analysis.sh
+++ scripts/static_analysis.sh
@@ -5,17 +5,24 @@ analyze_folder() {
     local section_name="$2"
 
     echo "\n\n+++++++++++++ ${section_name} File analysis +++++++++++++\n\n"
-
+    
     # File tree analysis
     tree -h -L 999 --du "$input_folder" >> output.txt
 
     echo "\n\n+++++++++++++ ${section_name} Image analysis +++++++++++++\n\n" >> output.txt
 
+    total_files=$(find "$input_folder" -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.gif" \) | wc -l)
+    processed_files=0
+
     find "$input_folder" -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.gif" \) -print0 | while IFS= read -r -d '' file; do
-        file_size=$(du -sh "$file" | cut -f1) # Pobiera rozmiar pliku za pomocą 'du' i wycina pierwszą kolumnę (rozmiar)
-        image_dimensions=$(exiftool -ImageSize -s3 "$file") # Pobiera wymiary obrazka
+        file_size=$(du -sh "$file" | cut -f1) 
+        image_dimensions=$(exiftool -ImageSize -s3 "$file") 
         echo "$file - $file_size - $image_dimensions"
+        processed_files=$((processed_files+1))
+        echo -ne "${section_name} Processing: $processed_files/$total_files files\r" >&2
     done >> output.txt
+
+    echo -e "\n" >&2
 }
 
 backend_static="$HOME/www/Quiero_regresar_a_casa/backend/main/nginx/static/"
@@ -30,6 +37,6 @@ analyze_folder "$backend_static" "Backend"
 # Analiza dla frontendu
 analyze_folder "$frontend_static" "Frontend"
 
-less -f -r output.txt;
+less -f -R output.txt;
 rm output.txt;
 
