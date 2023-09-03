718912d68e446ae15884a431749903c4eb89a7bf
murtrag <lukasz.czernik@gmail.com>
2023-09-03 12:27:41 +0200

    update settings

--- backend/main/main/settings/cross_origin.py
+++ backend/main/main/settings/cross_origin.py
@@ -1,16 +1,12 @@
 from main.settings.core import DEBUG
 
 CORS_ALLOW_CREDENTIALS = True
-ALLOWED_HOSTS = ['quieroregresaracasa.murtrag.repl.co', '*']
+ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = ['http://*', 'https://*']
-# This should be removed later on
-# ALLOWED_HOSTS.append('*')
-# CORS_ORIGIN_ALLOW_ALL = True
-# This should be removed later on
 
 if DEBUG is True:
     ALLOWED_HOSTS.append('*')
     CORS_ORIGIN_ALLOW_ALL = True
-    CORS_ORIGIN_WHITELIST = ('http://quiero-regresar.mx',)
+    CORS_ORIGIN_WHITELIST = ('http://quiero-regresar.mx', 'http://quiero-regresar.mx:81',)
 else:
     CORS_ORIGIN_WHITELIST = ('http://quiero-regresar.mx', 'http://quiero-regresar.mx:81',)
