04890713eb187ef82c5657598abeac14f0177c24
murtrag <lukasz.czernik@gmail.com>
2023-09-07 11:58:58 +0200

    ff

--- backend/main/main/settings/cross_origin.py
+++ backend/main/main/settings/cross_origin.py
@@ -2,11 +2,11 @@ from main.settings.core import DEBUG
 
 CORS_ALLOW_CREDENTIALS = True
 ALLOWED_HOSTS = ['*']
-CSRF_TRUSTED_ORIGINS = ['http://*', 'https://*']
+CSRF_TRUSTED_ORIGINS = ['http://quiero-regresar.mx:81']
 
 if DEBUG is True:
     ALLOWED_HOSTS.append('*')
     CORS_ORIGIN_ALLOW_ALL = True
-    CORS_ORIGIN_WHITELIST = ('http://quiero-regresar.mx', 'http://quiero-regresar.mx:81', 'http://localhost:8000',)
+    CORS_ORIGIN_WHITELIST = ['http://quiero-regresar.mx', 'http://quiero-regresar.mx:81', 'http://localhost:8000',]
 else:
-    CORS_ORIGIN_WHITELIST = ('http://quiero-regresar.mx', 'http://quiero-regresar.mx:81', 'http://localhost:8000',)
+    CORS_ORIGIN_WHITELIST = ['http://quiero-regresar.mx', 'http://quiero-regresar.mx:81', 'http://localhost:8000',]
